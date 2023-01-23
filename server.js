7922ba5937e7a250b0fd9181d0dca3fffc7ab2e6
cadienlake <115520010+cadienlake@users.noreply.github.com>
2023-01-23 17:49:02 -0600

    Fixed lowercase PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const api = require("./routes");
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -18,6 +18,4 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html
 // GET Route for notes page
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
-
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
-``
